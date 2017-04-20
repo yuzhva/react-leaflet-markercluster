d796f21ce151712333369b3a8df32025664cf6de
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2017-04-20 12:27:08 +0300

    [config] Use PropTypes from separate package

--- src/react-leaflet-markercluster.js
+++ src/react-leaflet-markercluster.js
@@ -1,4 +1,5 @@
-import React, {PropTypes, Children, cloneElement} from 'react';
+import React, {Children, cloneElement} from 'react';
+import PropTypes from 'prop-types';
 
 import {LayerGroup} from 'react-leaflet';
 import L from 'leaflet'
