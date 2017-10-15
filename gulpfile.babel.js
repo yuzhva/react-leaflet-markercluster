319f9d068e3b304962a65fe45c781cc1bac59e5a
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2017-10-15 21:06:02 +0300

    [refactoring] Improve syntax of config files according new ESlint rules

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,5 @@
 import gulp from 'gulp';
-import del from 'del'
+import del from 'del';
 import runSequence from 'run-sequence';
 import babel from 'gulp-babel';
 import sass from 'gulp-sass';
@@ -8,39 +8,39 @@ import uglifyJS from 'gulp-uglify';
 import uglifyCSS from 'gulp-clean-css';
 
 gulp.task('build', () => {
-  runSequence('dist:clean', 'dist:script', 'dist:style', 'uglify:script', 'uglify:style')
+  runSequence('dist:clean', 'dist:script', 'dist:style', 'uglify:script', 'uglify:style');
 });
 
-gulp.task('dist:clean', () => {
-  return del('./dist/**');
-});
+gulp.task('dist:clean', () => (
+  del('./dist/**')
+));
 
-gulp.task('dist:script', () => {
-  return gulp.src('./src/react-leaflet-markercluster.js')
-    .pipe(babel({presets: ['es2015', 'react']}))
-    .pipe(gulp.dest('./dist'));
-});
+gulp.task('dist:script', () => (
+  gulp.src('./src/react-leaflet-markercluster.js')
+    .pipe(babel({ presets: ['es2015', 'react'] }))
+    .pipe(gulp.dest('./dist'))
+));
 
-gulp.task('dist:style', () => {
-  return gulp.src('./src/style.scss')
+gulp.task('dist:style', () => (
+  gulp.src('./src/style.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
-});
+    .pipe(gulp.dest('./dist'))
+));
 
-gulp.task('uglify:script', () => {
-  return gulp.src('./dist/react-leaflet-markercluster.js')
+gulp.task('uglify:script', () => (
+  gulp.src('./dist/react-leaflet-markercluster.js')
     .pipe(uglifyJS())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./dist'));
-});
+    .pipe(gulp.dest('./dist'))
+));
 
-gulp.task('uglify:style', () => {
-  return gulp.src('./dist/style.css')
-    .pipe(uglifyCSS({compatibility: 'ie8'}))
+gulp.task('uglify:style', () => (
+  gulp.src('./dist/style.css')
+    .pipe(uglifyCSS({ compatibility: 'ie8' }))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./dist'));
-});
+    .pipe(gulp.dest('./dist'))
+));
 
-gulp.task('demo-app:clean', () => {
-  return del(['./index.html', './demo-app/dist/**']);
-});
+gulp.task('demo-app:clean', () => (
+  del(['./index.html', './demo-app/dist/**'])
+));
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,15 +1,15 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 module.exports = {
   entry: './demo-app/main.js',
   output: {
     path: __dirname,
-    filename: './demo-app/dist/scripts.js'
+    filename: './demo-app/dist/scripts.js',
   },
   devServer: {
     port: 8080,
-    contentBase: __dirname
+    contentBase: __dirname,
   },
   module: {
     rules: [
@@ -19,25 +19,25 @@ module.exports = {
         exclude: /node_modules/,
         query: {
           presets: [
-            ['es2015', {modules: false}],
-            'react'
-          ]
-        }
+            ['es2015', { modules: false }],
+            'react',
+          ],
+        },
       }, {
         test: /\.css$/,
-        loader:'style-loader!css-loader'
+        loader: 'style-loader!css-loader',
       }, {
         test: /\.scss$/,
         exclude: /components/,
         loader: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: 'css-loader!sass-loader'
-        })
-      }
-    ]
+          use: 'css-loader!sass-loader',
+        }),
+      },
+    ],
   },
   plugins: [
     new ExtractTextPlugin('./demo-app/dist/styles.css'),
-    new HtmlWebpackPlugin({template: './demo-app/index.template.html'})
-  ]
+    new HtmlWebpackPlugin({ template: './demo-app/index.template.html' }),
+  ],
 };
