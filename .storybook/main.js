ae545159c7d481e177fe70da13f1eeda9fa8c7b0
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2020-04-03 13:23:48 +0300

    build: clean-up npm module

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ['./**/*.stories.(js|mdx)'],
-  presets: ['@storybook/preset-scss'],
-  addons: [
-    '@storybook/addon-actions/register',
-    '@storybook/addon-docs',
-  ]
-};
