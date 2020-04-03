6267d00beafad260e054ba7058c0c477145e1b19
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2020-04-03 13:23:48 +0300

    build: clean-up npm module

--- .storybook/examples/helpers.js
+++
@@ -1,6 +0,0 @@
-import { action } from '@storybook/addon-actions';
-
-export const logAction = (actionName, ...args) => {
-  console.warn(actionName, ...args);
-  return action(actionName)(...args);
-};
