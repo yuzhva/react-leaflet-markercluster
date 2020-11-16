74de0b07a8930a504121d3a2810040667a8e310f
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2020-11-16 10:31:46 +0200

    refactor: implement review suggestions

--- src/react-leaflet-markercluster.js
+++ src/react-leaflet-markercluster.js
@@ -3,32 +3,32 @@ import L from 'leaflet';
 
 require('leaflet.markercluster');
 
-const MarkerClusterGroup = createPathComponent((
-  { children: _c, ...props },
-  ctx
-) => {
-  const clusterProps = {};
-  const clusterEvents = {};
+const MarkerClusterGroup = createPathComponent(
+  ({ children: _c, ...props }, ctx) => {
+    const clusterProps = {};
+    const clusterEvents = {};
 
-  // Splitting props and events to different objects
-  Object.entries(props).forEach(
-    ([propName, prop]) => propName.startsWith('on')
-      ? clusterEvents[propName] = prop
-      : clusterProps[propName] = prop
-  );
+    // Splitting props and events to different objects
+    Object.entries(props).forEach(([propName, prop]) =>
+      propName.startsWith('on')
+        ? (clusterEvents[propName] = prop)
+        : (clusterProps[propName] = prop)
+    );
 
-  // Creating markerClusterGroup Leaflet element
-  const markerClusterGroup = new L.markerClusterGroup(clusterProps);
+    // Creating markerClusterGroup Leaflet element
+    const markerClusterGroup = new L.markerClusterGroup(clusterProps);
 
-  // Initializing event listeners
-  Object.entries(clusterEvents).forEach(
-    ([eventAsProp, callback]) => {
+    // Initializing event listeners
+    Object.entries(clusterEvents).forEach(([eventAsProp, callback]) => {
       const clusterEvent = `cluster${eventAsProp.substring(2).toLowerCase()}`;
       markerClusterGroup.on(clusterEvent, callback);
-    },
-  );
+    });
 
-  return { instance: markerClusterGroup, context: { ...ctx, layerContainer: markerClusterGroup } };
-});
+    return {
+      instance: markerClusterGroup,
+      context: { ...ctx, layerContainer: markerClusterGroup },
+    };
+  }
+);
 
 export default MarkerClusterGroup;
