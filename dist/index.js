55ccf0da03ff4c97a5b58162cab38b6935fb8a98
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2024-10-30 02:05:06 +0200

    [beta] 4.0.0-rc1: react-leaflet v4 support (#210)

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,210 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports.default=void 0;var _core=require("@react-leaflet/core");var _leaflet=_interopRequireDefault(require("leaflet"));function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}function ownKeys(object,enumerableOnly){var keys=Object.keys(object);if(Object.getOwnPropertySymbols){var symbols=Object.getOwnPropertySymbols(object);if(enumerableOnly)symbols=symbols.filter((function(sym){return Object.getOwnPropertyDescriptor(object,sym).enumerable}));keys.push.apply(keys,symbols)}return keys}function _objectSpread(target){for(var i=1;i<arguments.length;i++){var source=arguments[i]!=null?arguments[i]:{};if(i%2){ownKeys(Object(source),true).forEach((function(key){_defineProperty(target,key,source[key])}))}else if(Object.getOwnPropertyDescriptors){Object.defineProperties(target,Object.getOwnPropertyDescriptors(source))}else{ownKeys(Object(source)).forEach((function(key){Object.defineProperty(target,key,Object.getOwnPropertyDescriptor(source,key))}))}}return target}function _defineProperty(obj,key,value){if(key in obj){Object.defineProperty(obj,key,{value:value,enumerable:true,configurable:true,writable:true})}else{obj[key]=value}return obj}function _slicedToArray(arr,i){return _arrayWithHoles(arr)||_iterableToArrayLimit(arr,i)||_unsupportedIterableToArray(arr,i)||_nonIterableRest()}function _nonIterableRest(){throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}function _unsupportedIterableToArray(o,minLen){if(!o)return;if(typeof o==="string")return _arrayLikeToArray(o,minLen);var n=Object.prototype.toString.call(o).slice(8,-1);if(n==="Object"&&o.constructor)n=o.constructor.name;if(n==="Map"||n==="Set")return Array.from(o);if(n==="Arguments"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n))return _arrayLikeToArray(o,minLen)}function _arrayLikeToArray(arr,len){if(len==null||len>arr.length)len=arr.length;for(var i=0,arr2=new Array(len);i<len;i++){arr2[i]=arr[i]}return arr2}function _iterableToArrayLimit(arr,i){if(typeof Symbol==="undefined"||!(Symbol.iterator in Object(arr)))return;var _arr=[];var _n=true;var _d=false;var _e=undefined;try{for(var _i=arr[Symbol.iterator](),_s;!(_n=(_s=_i.next()).done);_n=true){_arr.push(_s.value);if(i&&_arr.length===i)break}}catch(err){_d=true;_e=err}finally{try{if(!_n&&_i["return"]!=null)_i["return"]()}finally{if(_d)throw _e}}return _arr}function _arrayWithHoles(arr){if(Array.isArray(arr))return arr}function _objectWithoutProperties(source,excluded){if(source==null)return{};var target=_objectWithoutPropertiesLoose(source,excluded);var key,i;if(Object.getOwnPropertySymbols){var sourceSymbolKeys=Object.getOwnPropertySymbols(source);for(i=0;i<sourceSymbolKeys.length;i++){key=sourceSymbolKeys[i];if(excluded.indexOf(key)>=0)continue;if(!Object.prototype.propertyIsEnumerable.call(source,key))continue;target[key]=source[key]}}return target}function _objectWithoutPropertiesLoose(source,excluded){if(source==null)return{};var target={};var sourceKeys=Object.keys(source);var key,i;for(i=0;i<sourceKeys.length;i++){key=sourceKeys[i];if(excluded.indexOf(key)>=0)continue;target[key]=source[key]}return target}require("leaflet.markercluster");var MarkerClusterGroup=(0,_core.createPathComponent)((function(_ref,ctx){var _c=_ref.children,props=_objectWithoutProperties(_ref,["children"]);var clusterProps={};var clusterEvents={};Object.entries(props).forEach((function(_ref2){var _ref3=_slicedToArray(_ref2,2),propName=_ref3[0],prop=_ref3[1];return propName.startsWith("on")?clusterEvents[propName]=prop:clusterProps[propName]=prop}));var markerClusterGroup=new _leaflet.default.markerClusterGroup(clusterProps);Object.entries(clusterEvents).forEach((function(_ref4){var _ref5=_slicedToArray(_ref4,2),eventAsProp=_ref5[0],callback=_ref5[1];var clusterEvent="cluster".concat(eventAsProp.substring(2).toLowerCase());markerClusterGroup.on(clusterEvent,callback)}));return{instance:markerClusterGroup,context:_objectSpread(_objectSpread({},ctx),{},{layerContainer:markerClusterGroup})}}));var _default=MarkerClusterGroup;exports.default=_default;
+"use strict";
+function _typeof(o) {
+  "@babel/helpers - typeof";
+  return (
+    (_typeof =
+      "function" == typeof Symbol && "symbol" == typeof Symbol.iterator
+        ? function (o) {
+            return typeof o;
+          }
+        : function (o) {
+            return o &&
+              "function" == typeof Symbol &&
+              o.constructor === Symbol &&
+              o !== Symbol.prototype
+              ? "symbol"
+              : typeof o;
+          }),
+    _typeof(o)
+  );
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = void 0;
+var _core = require("@react-leaflet/core");
+var _leaflet = _interopRequireDefault(require("leaflet"));
+require("leaflet.markercluster");
+var _excluded = ["children"];
+function _interopRequireDefault(e) {
+  return e && e.__esModule ? e : { default: e };
+}
+function ownKeys(e, r) {
+  var t = Object.keys(e);
+  if (Object.getOwnPropertySymbols) {
+    var o = Object.getOwnPropertySymbols(e);
+    r &&
+      (o = o.filter(function (r) {
+        return Object.getOwnPropertyDescriptor(e, r).enumerable;
+      })),
+      t.push.apply(t, o);
+  }
+  return t;
+}
+function _objectSpread(e) {
+  for (var r = 1; r < arguments.length; r++) {
+    var t = null != arguments[r] ? arguments[r] : {};
+    r % 2
+      ? ownKeys(Object(t), !0).forEach(function (r) {
+          _defineProperty(e, r, t[r]);
+        })
+      : Object.getOwnPropertyDescriptors
+        ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t))
+        : ownKeys(Object(t)).forEach(function (r) {
+            Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r));
+          });
+  }
+  return e;
+}
+function _defineProperty(e, r, t) {
+  return (
+    (r = _toPropertyKey(r)) in e
+      ? Object.defineProperty(e, r, {
+          value: t,
+          enumerable: !0,
+          configurable: !0,
+          writable: !0,
+        })
+      : (e[r] = t),
+    e
+  );
+}
+function _toPropertyKey(t) {
+  var i = _toPrimitive(t, "string");
+  return "symbol" == _typeof(i) ? i : i + "";
+}
+function _toPrimitive(t, r) {
+  if ("object" != _typeof(t) || !t) return t;
+  var e = t[Symbol.toPrimitive];
+  if (void 0 !== e) {
+    var i = e.call(t, r || "default");
+    if ("object" != _typeof(i)) return i;
+    throw new TypeError("@@toPrimitive must return a primitive value.");
+  }
+  return ("string" === r ? String : Number)(t);
+}
+function _slicedToArray(r, e) {
+  return (
+    _arrayWithHoles(r) ||
+    _iterableToArrayLimit(r, e) ||
+    _unsupportedIterableToArray(r, e) ||
+    _nonIterableRest()
+  );
+}
+function _nonIterableRest() {
+  throw new TypeError(
+    "Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.",
+  );
+}
+function _unsupportedIterableToArray(r, a) {
+  if (r) {
+    if ("string" == typeof r) return _arrayLikeToArray(r, a);
+    var t = {}.toString.call(r).slice(8, -1);
+    return (
+      "Object" === t && r.constructor && (t = r.constructor.name),
+      "Map" === t || "Set" === t
+        ? Array.from(r)
+        : "Arguments" === t ||
+            /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t)
+          ? _arrayLikeToArray(r, a)
+          : void 0
+    );
+  }
+}
+function _arrayLikeToArray(r, a) {
+  (null == a || a > r.length) && (a = r.length);
+  for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e];
+  return n;
+}
+function _iterableToArrayLimit(r, l) {
+  var t =
+    null == r
+      ? null
+      : ("undefined" != typeof Symbol && r[Symbol.iterator]) || r["@@iterator"];
+  if (null != t) {
+    var e,
+      n,
+      i,
+      u,
+      a = [],
+      f = !0,
+      o = !1;
+    try {
+      if (((i = (t = t.call(r)).next), 0 === l)) {
+        if (Object(t) !== t) return;
+        f = !1;
+      } else
+        for (
+          ;
+          !(f = (e = i.call(t)).done) && (a.push(e.value), a.length !== l);
+          f = !0
+        );
+    } catch (r) {
+      (o = !0), (n = r);
+    } finally {
+      try {
+        if (!f && null != t.return && ((u = t.return()), Object(u) !== u))
+          return;
+      } finally {
+        if (o) throw n;
+      }
+    }
+    return a;
+  }
+}
+function _arrayWithHoles(r) {
+  if (Array.isArray(r)) return r;
+}
+function _objectWithoutProperties(e, t) {
+  if (null == e) return {};
+  var o,
+    r,
+    i = _objectWithoutPropertiesLoose(e, t);
+  if (Object.getOwnPropertySymbols) {
+    var s = Object.getOwnPropertySymbols(e);
+    for (r = 0; r < s.length; r++)
+      (o = s[r]),
+        t.includes(o) || ({}.propertyIsEnumerable.call(e, o) && (i[o] = e[o]));
+  }
+  return i;
+}
+function _objectWithoutPropertiesLoose(r, e) {
+  if (null == r) return {};
+  var t = {};
+  for (var n in r)
+    if ({}.hasOwnProperty.call(r, n)) {
+      if (e.includes(n)) continue;
+      t[n] = r[n];
+    }
+  return t;
+}
+function createMarkerCluster(_ref, context) {
+  var _c = _ref.children,
+    props = _objectWithoutProperties(_ref, _excluded);
+  var clusterProps = {};
+  var clusterEvents = {};
+  Object.entries(props).forEach(function (_ref2) {
+    var _ref3 = _slicedToArray(_ref2, 2),
+      propName = _ref3[0],
+      prop = _ref3[1];
+    return propName.startsWith("on")
+      ? (clusterEvents[propName] = prop)
+      : (clusterProps[propName] = prop);
+  });
+  var instance = new _leaflet.default.MarkerClusterGroup(clusterProps);
+  Object.entries(clusterEvents).forEach(function (_ref4) {
+    var _ref5 = _slicedToArray(_ref4, 2),
+      eventAsProp = _ref5[0],
+      callback = _ref5[1];
+    var clusterEvent = "cluster".concat(eventAsProp.substring(2).toLowerCase());
+    instance.on(clusterEvent, callback);
+  });
+  return {
+    instance: instance,
+    context: _objectSpread(
+      _objectSpread({}, context),
+      {},
+      { layerContainer: instance },
+    ),
+  };
+}
+var MarkerCluster = (0, _core.createPathComponent)(createMarkerCluster);
+var _default = (exports.default = MarkerCluster);
--- src/react-leaflet-markercluster.js
+++ src/react-leaflet-markercluster.js
@@ -1,32 +1,32 @@
-import L from 'leaflet'
-import { createPathComponent } from '@react-leaflet/core'
-import 'leaflet.markercluster'
+import { createPathComponent } from '@react-leaflet/core';
+import L from 'leaflet';
+import 'leaflet.markercluster';
 
 function createMarkerCluster({ children: _c, ...props }, context) {
-  const clusterProps = {}
-  const clusterEvents = {}
+  const clusterProps = {};
+  const clusterEvents = {};
   // Splitting props and events to different objects
   Object.entries(props).forEach(([propName, prop]) =>
     propName.startsWith('on')
       ? (clusterEvents[propName] = prop)
       : (clusterProps[propName] = prop)
-  )
-  const instance = new L.MarkerClusterGroup(clusterProps)
+  );
+  const instance = new L.MarkerClusterGroup(clusterProps);
 
   // Initializing event listeners
   Object.entries(clusterEvents).forEach(([eventAsProp, callback]) => {
-    const clusterEvent = `cluster${eventAsProp.substring(2).toLowerCase()}`
-    instance.on(clusterEvent, callback)
-  })
+    const clusterEvent = `cluster${eventAsProp.substring(2).toLowerCase()}`;
+    instance.on(clusterEvent, callback);
+  });
   return {
     instance,
     context: {
       ...context,
       layerContainer: instance,
     },
-  }
+  };
 }
 
-const MarkerCluster = createPathComponent(createMarkerCluster)
+const MarkerCluster = createPathComponent(createMarkerCluster);
 
-export default MarkerCluster
+export default MarkerCluster;
