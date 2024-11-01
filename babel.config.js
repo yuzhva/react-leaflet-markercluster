0d8bf1cd6366f1410c492cf84d6ce7de7bbe3800
charlieforward9 <charlesrichardsonusa@gmail.com>
2024-11-01 10:42:52 -0700

    # This is a combination of 7 commits. # This is the 1st commit message:
    fix: add support for esm
    
    # This is the commit message #2:
    
    fix: configure support for esm
    
    # This is the commit message #3:
    
    fix: add exports
    
    # This is the commit message #4:
    
    fix: export syntax
    
    # This is the commit message #5:
    
    fix: styles export
    
    # This is the commit message #6:
    
    fix: export
    
    # This is the commit message #7:
    
    fix: support esm

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,21 @@
 const babelConfig = {
   presets: [
     [
-      '@babel/preset-env',
+      "@babel/preset-env",
       {
+        modules: false,
         targets: {
-          node: 'current',
-          chrome: '60',
-          firefox: '60',
-          safari: '11.1',
-          edge: '17',
-          ie: '11',
+          node: "current",
+          chrome: "60",
+          firefox: "60",
+          safari: "11.1",
+          edge: "17",
+          ie: "11",
         },
       },
     ],
-    '@babel/preset-react',
+    "@babel/preset-react",
   ],
 };
 
-module.exports = babelConfig;
+export default babelConfig;
