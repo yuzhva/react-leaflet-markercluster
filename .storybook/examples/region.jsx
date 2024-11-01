254a03ba56a808bd1ec9f49d7b0e7a7c09d6a6a0
charlieforward9 <charlesrichardsonusa@gmail.com>
2024-10-31 19:17:00 -0700

    feat: incorporate bulk into region example

--- .storybook/examples/region.jsx
+++ .storybook/examples/region.jsx
@@ -6,6 +6,10 @@ import MarkerClusterGroup from "../../src/react-leaflet-markercluster";
 import { MAP_ZOOM, MAP_MAX_ZOOM, MAP_CENTER_COORDINATES } from "./constants";
 import "./styles.scss";
 
+function randomCoords() {
+  return [160 * Math.random() - 80, 360 * Math.random() - 180];
+}
+
 // Extend L.Polygon to include a clustering-compatible getLatLng method
 // Credits to https://github.com/Leaflet/Leaflet.markercluster/issues/612#issuecomment-242929562
 const ClusterableRegion = createPathComponent(function createClusterableRegion(
@@ -53,41 +57,29 @@ const RegionExample = () => (
     />
 
     <MarkerClusterGroup animate={true}>
-      <ClusterableRegion
-        coordinates={[
-          [52.9, 16.4],
-          [52.8, 16.2],
-          [53.0, 16.0],
-        ]}
-      />
-      <ClusterableRegion
-        coordinates={[
-          [48.5, 15.0],
-          [48.8, 15.3],
-          [48.6, 15.5],
-        ]}
-      />
-      <ClusterableRegion
-        coordinates={[
-          [49.5, 22.0],
-          [49.8, 22.3],
-          [49.3, 22.2],
-        ]}
-      />
-      <ClusterableRegion
-        coordinates={[
-          [52.5, 25.0],
-          [52.5, 25.4],
-          [52.3, 25.2],
-        ]}
-      />
-      <ClusterableRegion
-        coordinates={[
-          [51.5, -0.09],
-          [51.3, -0.1],
-          [51.4, -0.2],
-        ]}
-      />
+      {Array.from({ length: 42069 }, (_, i) => {
+        const centerCoords = randomCoords();
+        return (
+          <ClusterableRegion
+            key={i}
+            coordinates={[
+              centerCoords,
+              [
+                centerCoords[0] - Math.random(),
+                centerCoords[1] - Math.random(),
+              ],
+              [
+                centerCoords[0] - Math.random(),
+                centerCoords[1] + Math.random(),
+              ],
+              [
+                centerCoords[0] + Math.random(),
+                centerCoords[1] + Math.random(),
+              ],
+            ]}
+          />
+        );
+      })}
     </MarkerClusterGroup>
   </MapContainer>
 );
